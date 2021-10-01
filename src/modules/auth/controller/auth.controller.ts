51f9c89771d1b1082008f7c642754e3606cda67f
xursand7777 <khursand.khaitbaev@mail.ru>
2021-10-01 19:36:54 +0500

    closed issue #5

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Post, UseGuards, Request } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "../service/auth.service";
-import { AdminRepository } from "../../admin/service/admin.repository";
+
+import { Connection, Repository } from "typeorm";
+import { Admin } from "../../admin/model/admin.entity";
 
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService, private adminRepository: AdminRepository) {
 
+  private adminRepository:Repository<Admin>
+  constructor(private authService: AuthService, private connection: Connection) {
+    this.adminRepository = this.connection.getRepository(Admin)
   }
+
+
+
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
@@ -25,7 +32,7 @@ export class AuthController {
   @UseGuards(AuthGuard('jwt'))
   @Post('refresh')
   async refresh(@Request() req) {
-    const admin = await this.adminRepository.find(req.user.id)
+    const admin = await this.adminRepository.findOne(req.user.id)
     return this.authService.login(admin)
   }
 }
